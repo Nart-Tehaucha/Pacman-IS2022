f6a9651e8ec7efad4f021a6db88edae76b5c0181
nart <nart@nart-ThinkPad-E490>
2021-12-17 12:10:25 +0200

    test

--- Pacman-master/src/controllers/Main.java
+++ Pacman-master/src/controllers/Main.java
@@ -4,13 +4,12 @@ import views.PacWindow;
 
 public class Main {
 	
-
-	
 	// Main class, opens the main screen
     public static void main(String[] args) throws Exception {
 //    	 new StartWindow();
     	//PlayersController.createJSON();
     	JFXLauncher.main(args);
+    	//123
     	//new PacWindow("Nart");
     	//Test.main(args);
     	//System.out.println(new SysData().readQuestionsJSON().toString());
